25fbc8c1a390966ca4f6f3a8a29ca6257b2720ba
Anthony Beckford <abeckford532@gmail.com>
2022-07-27 18:44:12 -0400

    Add files via upload

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Anthony Beckford on 5/11/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
